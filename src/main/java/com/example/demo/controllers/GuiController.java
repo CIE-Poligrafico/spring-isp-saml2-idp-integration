2bd180816a607df64ea83351768b33bc8a4f8386
Antonio Spa <antonio.defazio@virgilio.it>
2021-05-19 10:35:22 +0200

    Add JsonApi

---
+++ src/main/java/com/example/demo/controllers/GuiController.java
@@ -0,0 +1,34 @@
+package com.example.demo.controllers;
+
+import java.util.Date;
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.MessageSource;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+
+
+
+
+
+
+
+@Controller
+public class GuiController {
+
+	 
+	 @GetMapping("/cielogs")
+	 public String logs(){
+		 return "index";
+	 }
+
+	
+}
